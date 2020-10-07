099421e8e8334edc2573dd8cce6509b18994c381
Marco <marcon7@uw.edu>
2020-10-07 15:33:30 -0700

    Finish problem 1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,6 +3,19 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
+        int right = data.length - 1;
+        int left = 0;
+        while (left<=right) {
+            int mid = (right + left) / 2;
+
+                if (target == data[mid]) {
+                    return mid;
+                } else if (target > data[mid]) {
+                    left = mid + 1;
+                } else
+                    right = mid - 1;
+            }
         return -1; // Place holder
+        }
     }
-}
+// help from geekforgeeks (link in assignment page)
